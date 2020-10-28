3ed311d68c3a527ba501ed45e4b8cc676e929d29
Lukasz Czaplinski <git@scoiatael.dev>
2020-10-28 18:35:44 +0100

    Let's stick with Heroku default

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MamWirusa.MixProject do
     [
       app: :mam_wirusa,
       version: "0.1.0",
-      elixir: "~> 1",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
