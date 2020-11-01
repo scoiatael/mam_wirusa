8fc744dab851b6ffcf5c14e5000a3f6f79fb02d6
Lukasz Czaplinski <git@scoiatael.dev>
2020-11-01 16:20:55 +0100

    Show marker for precise locations

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -1,9 +1,30 @@
-const Marker = ({lng, lat, acc}) =>
-    L.circle([lat, lng], {
+function Marker({lng, lat, acc}) {
+    const circle = L.circle([lat, lng], {
         color: '#ff000009',
         fillColor: '#f03',
         fillOpacity: 0.5,
         radius: 20000 * 2**6 * 1/2**acc
     })
+    if (acc < 11) {
+        return circle;
+    }
+    const marker = L.marker([lat, lng])
+    const self = {
+        addTo: (map) => {
+            circle.addTo(map);
+            marker.addTo(map)
+            return self
+        },
+        bindPopup: (text) => {
+            circle.bindPopup(text)
+            return self
+        },
+        remove: () => {
+            circle.remove()
+            marker.remove()
+        }
+    }
+    return self
+}
 
 export default Marker
